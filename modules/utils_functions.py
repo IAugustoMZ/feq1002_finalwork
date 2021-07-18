3431111f4a6fa6f828c2be82085a6cd8e429f228
IAugustoMZ <i.augustomz@gmail.com>
2021-07-18 19:54:15 -0300

    Saturation Properties Models from Ammonia and Water trained. All saturation properties models validated

--- modules/utils_functions.py
+++ modules/utils_functions.py
@@ -1,11 +1,22 @@
+import joblib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 import json
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 
 # utilitary functions
 class utils:
+
+    # seed for reproducibility
+    RANDOM_SEED = 1002
+
+    # filepath for saving models (depends on the file system architecture)
+    MODEL_SAVE_FILEPATH = '../models'
 
     # construct method
     def __init__(self) -> None:
@@ -54,17 +65,18 @@ class utils:
         expected = values_list[0]
         predicted = values_list[1]
 
-        # calculate residues
-        residues = [e-p for e,p in zip(expected, predicted)]
-
+        # create dataframe for residue calculation
+        res_df = pd.DataFrame(expected, columns = ['real'])
+        res_df['predicted'] = predicted
+        res_df['residues'] = res_df['real'] - res_df['predicted']
 
         # build plots
         fig = plt.figure(figsize=(14,7))
 
         # expected vs predicted
         ax1 = fig.add_subplot(1,2,1)
-        ax1.plot(expected, expected, 'r-', label = 'Perfect Prediction')
-        ax1.plot(expected, predicted, 'ko', label = 'True Prediction')
+        ax1.plot(res_df['real'], res_df['real'], 'r-', label = 'Perfect Prediction')
+        ax1.plot(res_df['real'], res_df['predicted'], 'ko', label = 'True Prediction')
         ax1.set_xlabel(var_name + '\nExpected Values', size = 14)
         ax1.set_ylabel(var_name + '\nPredicted Values', size = 14)
         ax1.set_title('Prediction Assessment\n' + var_name, size = 16)
@@ -73,8 +85,151 @@ class utils:
 
         # residues histogram
         ax2 = fig.add_subplot(1,2,2)
-        ax2.hist(residues, color = 'lightgreen', alpha = 0.5)
+        ax2.hist(res_df['residues'], color = 'lightgreen', alpha = 0.5)
         ax2.set_xlabel(var_name + '\nResidues', size = 14)
         ax2.set_ylabel('Frequency', size = 14)
         ax2.set_title('Prediction Errors Distribution\n' + var_name, size = 16)
         ax2.grid()
+
+    # model training and evaluation
+    def model_training_selection(self, x, y, property_name, times_to_interpolate = 1, decision_score = 'mae'):
+        """
+        performs model training, cross validation and selection using
+        Leave One Out cross validation. The models tested are LinearRegression or DecisionTrees
+        
+        args: predictors arrays and target array, string containing property name,
+            integer defining the number os successive linear interpolations,
+            string containing the score that will be used to select model (default is 'MAE')
+        
+        returns: save best model and average performance index
+        """
+
+        # create model dictionary
+        model_dict = {
+            1: {
+                'name': 'Linear Regression',
+                'model': LinearRegression(),
+                'cross_val_results': 0,
+                'test_perf_index': {
+                    'mae': 0,
+                    'rmse': 0,
+                    'r2': 0
+                },
+            },
+            2: {
+                'name': 'Decision Tree Regressor',
+                'model': DecisionTreeRegressor(random_state=self.RANDOM_SEED),
+                'cross_val_results': 0,
+                'test_perf_index':{
+                    'mae': 0,
+                    'rmse': 0,
+                    'r2': 0
+                }
+            }
+        }
+
+        # perform data augmentation using prior chemical engineering knowledge
+        x, y = self.data_augmentation(x, y, times_to_interpolate=times_to_interpolate)
+
+        # performing train-test split to create a holdout set
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=self.RANDOM_SEED)
+
+        # using the train set, perform K fold cross validation
+        cv = KFold(n_splits = 10, shuffle=True, random_state=self.RANDOM_SEED)
+
+        # train and cross validate each model
+        for item in model_dict:
+
+            # selecting model
+            model = model_dict[item]['model']
+
+            # training model using cross validation
+            scores = cross_val_score(model, x_train, y_train, scoring='neg_mean_absolute_error',
+                    cv = cv)
+
+            # store results in model_dictionary
+            model_dict[item]['cross_val_results'] = np.mean(abs(scores))
+
+            # log of results
+            print('[INFO] ' + model_dict[item]['name'] + ' cross validation MAE: %.4f'%(np.mean(abs(scores))))
+
+            # after cross validation, train model in full train set
+            model_dict[item]['model'] = model_dict[item]['model'].fit(x_train, y_train)
+
+            # make predictions at test set
+            yhat = model_dict[item]['model'].predict(x_test)
+
+            # calculate performance parameters
+            model_dict[item]['test_perf_index']['mae'] = mean_absolute_error(y_test, yhat)
+            model_dict[item]['test_perf_index']['rmse'] = np.sqrt(mean_squared_error(y_test, yhat))
+            model_dict[item]['test_perf_index']['r2'] = r2_score(y_test, yhat)
+
+            # log of results
+            print('[INFO] ' + model_dict[item]['name'] + ' holdout set MAE: %.4f'%(model_dict[item]['test_perf_index']['mae']))
+            print('[INFO] ' + model_dict[item]['name'] + ' holdout set RMSE: %.4f'%(model_dict[item]['test_perf_index']['rmse']))
+            print('[INFO] ' + model_dict[item]['name'] + ' holdout set R2: %.4f'%(model_dict[item]['test_perf_index']['r2']))
+            print('-------------------------------------------------------------------------------------------------')
+
+        # after training and evaluating models, select based on desired performance score
+        if decision_score in ['mae', 'rmse']:
+            # select model based on lower mae or rmse
+            BEST_SCORE = 1000
+            for item in model_dict:
+                if model_dict[item]['test_perf_index'][decision_score] < BEST_SCORE:
+                    BEST_SCORE = model_dict[item]['test_perf_index'][decision_score]
+                    BEST_MODEL = model_dict[item]['model']
+                    model_name = model_dict[item]['name'].lower().replace(' ', '') + '_' + property_name + '.m'
+
+        elif decision_score in ['r2']:
+            # select model based on higher r2 score
+            BEST_SCORE = 0
+            for item in model_dict:
+                if model_dict[item]['test_perf_index'][decision_score] > BEST_SCORE:
+                    BEST_SCORE = model_dict[item]['test_perf_index'][decision_score]
+                    BEST_MODEL = model_dict[item]['model']
+                    model_name = model_dict[item]['name'].lower().replace(' ', '') + '_' + property_name + '.m'
+        
+        # after selecting the model, print saving information and save the model
+        print('[INFO] Saving ' + model_name[:-2])
+        joblib.dump(BEST_MODEL, os.path.join(self.MODEL_SAVE_FILEPATH, model_name))
+
+    # data augmentation through interpolation
+    def data_augmentation(self, x, y, sort_var_name = 't', times_to_interpolate = 1):
+        """
+        performs data augmentation based on prior knowledge of chemical engineering
+        which states that if we have a thermodynamic relation between two intensive variables X and Y,
+        and provided that we have at least two experimental points from this relation - (X1, Y1) and (X2, Y2), then
+        the value of Y = (Y1 + Y2)/2 can be estimated by a model evaluated at X = (X1 + X2)/2
+        This method extends this knowledge by calculating the average of experimental points iteratively and thus,
+        promoting a kind of data augmentation.
+
+        args: predictors matrix, target matrix, sorting variable name, desired number of successive interpolations
+
+        returns: augmented dataset of predictors and targets
+        """
+        # joining sorting variable in target dataset
+        y = y.join(x[sort_var_name])
+
+        # ensure sorting before begin interpolations
+        x = x.sort_values(by=[sort_var_name])
+        y = y.sort_values(by=[sort_var_name])
+
+        # loop to calculate successive interpolations
+        for i in range(times_to_interpolate):
+            
+            # interpolate the predictors matrix and the target matrix
+            x_aux = x.rolling(2).mean().dropna()
+            y_aux = y.rolling(2).mean().dropna()
+
+            # concatenate the augmented data to original values
+            x = pd.concat([x, x_aux])
+            y = pd.concat([y, y_aux])
+
+            # sort datasets to prepare them for new interpolation
+            x = x.sort_values(by=[sort_var_name]).reset_index().drop(['index'], axis = 1)
+            y = y.sort_values(by=[sort_var_name]).reset_index().drop(['index'], axis = 1)
+
+        # drop unused columns from target dataset
+        y = y.drop([sort_var_name], axis = 1)
+
+        return x, y
--- modules/validation.py
+++ modules/validation.py
@@ -1,13 +1,18 @@
-import pandas as pd
-import numpy as np
 import os
 import json
+import joblib
 import property
 import utils_functions
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 # validation methods
 class validate():
+
+    # folder containing models (this file system architecture)
+    MODEL_SAVE_FILEPATH = '../models'
 
     # constructor method
     def __init__(self) -> None:
@@ -87,3 +92,86 @@ class validate():
 
         # plot prediction analysis
         ut.scatter_model_residues(values_list, var_name)
+
+    # saturation properties
+    def saturation_properties(self, data, fluid_code, property):
+        """
+        method to validate the machine learning models to predict saturation properties using 
+        temperature and pressure as inputs
+
+        args: experimental data, fluid code representing the desired fluid, the desired property to 
+            be validated
+
+        returns: plots comparing the experimental data to predicted values
+        """
+
+        # create a dictionary to select models
+        property_dict = {
+            'v': 'Volume',
+            'h': 'Enthalpy',
+            's': 'Entropy'
+        }
+
+        # import fluid name
+        comp_name = self.fluid_dict[str(fluid_code)]['name']
+
+        # concatenate property + fluid name
+        query_str = property_dict[property] + '_' + comp_name
+
+        # load list of models
+        model_list = os.listdir(self.MODEL_SAVE_FILEPATH)
+
+        ######################################### SATURATED LIQUID PROPERTIES ############################################
+        
+        # create dataframe to receive predictions
+        pred_liq = data.loc[data['x']==0, ['t', 'p']]
+
+        # query full model list by state, property and fluid
+        query_str_liq = 'SatLiquid'+query_str
+        model_selected = [k for k in model_list if k.find(query_str_liq) != -1]
+
+        # load queried model
+        model = joblib.load(os.path.join(self.MODEL_SAVE_FILEPATH, model_selected[0]))
+
+        # predict the desired property using temperature and pressure
+        pred_liq[property] = model.predict(pred_liq[['t', 'p']])
+
+        ######################################### SATURATED VAPOR PROPERTIES #############################################
+
+        # create dataframe to receive predictions
+        pred_vap = data.loc[data['x']==1, ['t', 'p']]
+
+        # query full model list by state, property and fluid
+        query_str_vap = 'SatVapor'+query_str
+        model_selected = [k for k in model_list if k.find(query_str_vap) != -1]
+
+        # load queried model
+        model = joblib.load(os.path.join(self.MODEL_SAVE_FILEPATH, model_selected[0]))
+
+        # predict the desired property using temperature and pressure
+        pred_vap[property] = model.predict(pred_vap[['t', 'p']])
+
+        ###################################################################################################################
+
+        # plot saturation curves
+        fig = plt.figure(figsize=(14,7))
+
+        # saturation propery as function of temperature
+        ax1 = fig.add_subplot(1,2,1)
+        ax1.plot(data[property], data['t'],'ko', label = 'Experimental Data')
+        ax1.plot(pred_liq[property], pred_liq['t'],'r-', label = 'Model Prediction - Saturated Liquid')
+        ax1.plot(pred_vap[property], pred_vap['t'],'r-', label = 'Model Prediction - Saturated Vapor')
+        ax1.set_xlabel(property_dict[property] + ' - [' + self.fluid_dict['prop_units'][property] + ']', size = 14)
+        ax1.set_ylabel('Temperature - [' + self.fluid_dict['prop_units']['T'] + ']', size = 14)
+        ax1.legend(prop={'size': 12})
+        ax1.grid()
+
+        # saturation propery as function of pressure
+        ax2 = fig.add_subplot(1,2,2)
+        ax2.plot(data[property], data['p'],'ko', label = 'Experimental Data')
+        ax2.plot(pred_liq[property], pred_liq['p'],'r-', label = 'Model Prediction - Saturated Liquid')
+        ax2.plot(pred_vap[property], pred_vap['p'],'r-', label = 'Model Prediction - Saturated Vapor')
+        ax2.set_xlabel(property_dict[property] + ' - [' + self.fluid_dict['prop_units'][property] + ']', size = 14)
+        ax2.set_ylabel('MPa - [' + self.fluid_dict['prop_units']['P'] + ']', size = 14)
+        ax2.legend(prop={'size': 12})
+        ax2.grid()        
